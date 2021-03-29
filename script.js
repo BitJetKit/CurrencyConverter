253a9869b4314d0cb7a89443103ab1873375eee9
Jonathan M. Kelly <bitjetkit@gmail.com>
2021-03-29 00:59:26 -0500

    decl the currency variables

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-  
+  // Declare the currency variables.
+
+  var startingCurrency = 'USD';
+  var startingNumber = 1;
+  var targetCurrency = 'INR';
+  var targetNumber;
+  var url;
+
+  convertCurrency(startingCurrency, startingNumber, targetCurrency, targetNumber)
 });  
