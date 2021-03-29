b166577544be6929a93263779d1ff88bb71cceca
Jonathan M. Kelly <bitjetkit@gmail.com>
2021-03-29 01:08:26 -0500

    Decrease spacing

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   // Declare the currency variables.
-
   var startingCurrency = 'USD';
   var startingNumber = 1;
   var targetCurrency = 'INR';
@@ -10,15 +9,20 @@ $(document).ready(function () {
   convertCurrency(startingCurrency, startingNumber, targetCurrency, targetNumber)
 
   // Get the starting currency value.
-
   $("#starting").change(function (){
     // This is the starting currency.
-
     startingCurrency = $(this).children("option:selected").val();
 
     // Call the currency converting function.
-
     convertCurrency(startingCurrency, startingNumber, targetCurrency, targetNumber)
   });
-  
+
+  // Get the starting currency number.
+  $("#startingNumber").change(function(){
+    // This is the base number.
+    startingNumber = $(this).val()
+
+    // Call the currency converting function
+
+  })
 });  
